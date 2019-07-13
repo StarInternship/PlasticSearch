3fc2e5844e86193af59c3289f734986d37a04465
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-13 17:46:19 +0430

    implement develop in fuzzyTokenizer

--- src/models/tokenizer/FuzzySearchTokenizer.java
+++ src/models/tokenizer/FuzzySearchTokenizer.java
@@ -1,11 +1,46 @@
 package models.tokenizer;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class FuzzySearchTokenizer implements Tokenizer {
+    private static List<Character> characters = new ArrayList<>();
+
+    static {
+        for (char c = 'a'; c <= 'z'; c++) {
+            characters.add(c);
+        }
+        for (char c = '0'; c < '9'; c++) {
+            characters.add(c);
+        }
+    }
 
     @Override
     public LinkedList<String> develop(String token) {
+        LinkedList<String> developedTokens = new LinkedList<>();
+
+        developedTokens.addAll(insert(token));
+        developedTokens.addAll(delete(token));
+        developedTokens.addAll(substitute(token));
+
+        return developedTokens;
+    }
+
+    private LinkedList<String> delete(String token) {
+        LinkedList<String> deletes = new LinkedList<>();
+
+        for (int i = 0; i < token.length(); i++) {
+            deletes.add(token.substring(0, i) + token.substring(i + 1));
+        }
+        return deletes;
+    }
+
+    private LinkedList<String> insert(String token) {
+        return null;
+    }
+
+    private LinkedList<String> substitute(String token) {
         return null;
     }
 }
