89d85ea42986b34581e04dd7f7ad54d236102aa7
hadi <atomaton00@gmail.com>
2019-07-13 12:57:45 +0430

    complete exact search

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import controller.Controller;
 public class Main {
 
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Controller controller = new Controller(args[0]);
         controller.main();
     }
 }
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,12 +1,28 @@
 package controller;
 
+import models.analyzer.ExactSearchAnalyzer;
+import models.search.Search;
+import models.tokenizer.ExactSearchTokenizer;
 import view.FileView;
 import view.View;
 
-public class Controller {
-    View view = new View();
-    FileView fileView = new FileView("");
-    public void main() {
+import java.io.IOException;
 
+public class Controller {
+    private View view = new View();
+    private FileView fileView;
+    private final String PATH;
+
+    public Controller(String PATH) {
+        this.PATH = PATH;
+    }
+
+    public void main() throws IOException {
+        fileView = new FileView(PATH);
+        Search search = new Search(new ExactSearchAnalyzer(), new ExactSearchTokenizer());
+        search.preprocess(fileView.readFiles());
+        while (true) {
+            view.showResult(search.search(view.readNextLine()));
+        }
     }
 }
