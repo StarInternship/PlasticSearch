e3b9da98848dc1d99a095668f21cc2aad582f7b3
hadi <atomaton00@gmail.com>
2019-07-13 12:31:32 +0430

    add controller

---
+++ src/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+import view.FileView;
+import view.View;
+
+public class Controller {
+    View view = new View();
+    FileView fileView = new FileView("");
+    public void main() {
+
+    }
+}
