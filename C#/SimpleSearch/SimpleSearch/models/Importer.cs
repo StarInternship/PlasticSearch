9b8b7ef7fe9e5924d3d258667a4842d40bb54c06
hadi <atomaton00@gmail.com>
2019-07-14 07:11:08 +0430

    FileImporter

---
+++ C#/SimpleSearch/SimpleSearch/models/Importer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSearch.models
+{
+    class Importer
+    {
+        private readonly string filesPath = "test_files";
+
+        public IDictionary<string, string> ReadFiles()
+        {
+            Dictionary<string, string> files = new Dictionary<string, string>();
+            ReadFile(filesPath,files);
+            return files;
+        }
+
+        public void ReadFile(string path,Dictionary<string , string> filesDictionary)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                foreach (string filePath in files)
+                {
+                    ReadFile(filePath, filesDictionary);
+                }
+                return;
+            }
+            if (File.Exists(path))
+            {
+                string text = File.ReadAllText(path);
+                filesDictionary.Add(path, text);
+            }
+        }
+    }
+}
