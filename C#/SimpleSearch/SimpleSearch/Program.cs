5d7e84780b72fb05d09cd7cb4d7df1a9b16502e3
hadi <atomaton00@gmail.com>
2019-07-14 15:21:04 +0430

    connect console and tokenizer

--- C#/SimpleSearch/SimpleSearch/Program.cs
+++ C#/SimpleSearch/SimpleSearch/Program.cs
@@ -5,22 +5,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SimpleSearch.models;
+using SimpleSearch.models.tokenizer;
 
 namespace SimpleSearch
 {
     class Program
     {
+        static Tokenizer exactSearchTokenizer = new ExactSearchTokenizer();
+        static Tokenizer ngramSearchTokenizer = new NgramSearchTokenizer();
+
         static void Main(string[] args)
         {
-            Importer importer = new Importer();
-            Dictionary<string, string> files = importer.ReadFiles();
 
-            foreach (KeyValuePair<string, string> entry in files)
+
+            while (true)
             {
-                Console.WriteLine(entry.Key + " 's text = " + entry.Value);
+                string query = Console.ReadLine();
+
+                
+
             }
 
-            Console.ReadLine();
+        }
+
+        static void preprocess()
+        {
+            Importer importer = new Importer();
+
+            Dictionary<string, string> files = importer.ReadFiles();
+
+            foreach (KeyValuePair<string, string> pair in files)
+            {
+                string cleanText = ngramSearchTokenizer.CleanText(pair.Value);
+                //TODO: ahmad:)
+            }
         }
     }
 }
