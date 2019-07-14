9988df18415677878d6b62a690b24538373f7d0a
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-14 10:36:35 +0430

    start c#

---
+++ C#/SimpleSearch/SimpleSearch/models/Tokenizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSearch.models
+{
+    abstract class Tokenizer
+    {
+        public string CleanText(string text) => text.ToLower();
+
+        public abstract ISet<string> Tokenize(string text);
+
+        public abstract LinkedList<string> Develope(string token);
+    }
+}
