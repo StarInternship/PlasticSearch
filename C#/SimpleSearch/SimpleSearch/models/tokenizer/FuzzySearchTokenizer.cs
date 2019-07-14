10fecbb071dfe11511cece50dea41adf9785cd91
ahmadsalimi <ahsalimi@ce.sharif.edu>
2019-07-14 11:12:31 +0430

    add Fuzzy tokenizer (not implemented)

---
+++ C#/SimpleSearch/SimpleSearch/models/tokenizer/FuzzySearchTokenizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSearch.models.tokenizer
+{
+    class FuzzySearchTokenizer : Tokenizer
+    {
+        private static readonly int FUZZINESS = 1;
+        private static readonly IList<char> characters = new List<char>();
+
+        public override ISet<string> Tokenize(string text)
+        {
+            throw new NotImplementedException();
+        }
+        public override LinkedList<string> Develope(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
